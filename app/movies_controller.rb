bd5593721701210b0c10646998974ba92b2ede3a
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-15 20:47:02 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -19,7 +19,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.new(attributes)
+  movie = Movie.create(attributes)
 end
 
 def can_be_created_in_a_block(title, year)
